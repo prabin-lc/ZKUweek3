efe4b1e68d715d4d31f6b41d4bb45df4fb0a6314
Prabin Lamichhane <mail.prabinlamichhane@gmail.com>
2022-05-26 02:42:03 +0545

    week 3 completed

--- Part1/test/mastermind-test.js
+++ Part1/test/mastermind-test.js
@@ -1,1 +1,129 @@
 //[assignment] write your own unit test to show that your Mastermind variation circuit is working as expected
+
+const chai = require("chai");
+const path = require("path");
+
+const wasm_tester = require("circom_tester").wasm;
+const buildPoseidon = require("circomlibjs").buildPoseidon;
+
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString(
+  "21888242871839275222246405745257275088548364400416034343698204186575808495617"
+);
+const Fr = new F1Field(exports.p);
+
+const assert = chai.assert;
+
+describe("Mastermind Variation test", function () {
+  let poseidon;
+  let F;
+
+  it("Should give 3 hits for 3 correct guesses of colors and their positions", async () => {
+    poseidon = await buildPoseidon();
+    F = poseidon.F;
+
+    // our solutions will be 2,4,5
+    const privSolnA = 2;
+    const privSolnB = 4;
+    const privSolnC = 5;
+
+    // salt is taken as a random number
+    const privSalt = Math.floor(Math.random() * 10 ** 10);
+    //console.log(privSalt);
+
+    // public solution hash
+    const pubSolnHash = poseidon([privSalt, privSolnA, privSolnB, privSolnC]);
+
+    const circuit = await wasm_tester(
+      "contracts/circuits/MastermindVariation.circom"
+    );
+    await circuit.loadConstraints();
+
+    // this input corresponds to all correct guesses and gives 3 hits
+    const INPUT = {
+      pubGuessA: 2,
+      pubGuessB: 4,
+      pubGuessC: 5,
+      pubNumHit: 3,
+      pubNumBlow: 0,
+      pubSolnHash: F.toObject(pubSolnHash),
+      privSolnA: privSolnA,
+      privSolnB: privSolnB,
+      privSolnC: privSolnC,
+      privSalt: privSalt,
+    };
+
+    const witness = await circuit.calculateWitness(INPUT, true);
+
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+  });
+
+  it("Should give 1 hits and 1 blows", async () => {
+    poseidon = await buildPoseidon();
+    F = poseidon.F;
+
+    const privSolnA = 1;
+    const privSolnB = 2;
+    const privSolnC = 3;
+
+    const privSalt = Math.floor(Math.random() * 10 ** 10);
+
+    const pubSolnHash = poseidon([privSalt, privSolnA, privSolnB, privSolnC]);
+
+    const circuit = await wasm_tester(
+      "contracts/circuits/MastermindVariation.circom"
+    );
+    await circuit.loadConstraints();
+    const INPUT = {
+      pubGuessA: 1,
+      pubGuessB: 3,
+      pubGuessC: 4,
+      pubNumHit: 1,
+      pubNumBlow: 1,
+      pubSolnHash: F.toObject(pubSolnHash),
+      privSolnA: privSolnA,
+      privSolnB: privSolnB,
+      privSolnC: privSolnC,
+      privSalt: privSalt,
+    };
+
+    const witness = await circuit.calculateWitness(INPUT, true);
+
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+  });
+
+  it("Should give 0 hits and 2 blows", async () => {
+    poseidon = await buildPoseidon();
+    F = poseidon.F;
+
+    const privSolnA = 1;
+    const privSolnB = 2;
+    const privSolnC = 3;
+
+    const privSalt = Math.floor(Math.random() * 10 ** 10);
+
+    const pubSolnHash = poseidon([privSalt, privSolnA, privSolnB, privSolnC]);
+
+    const circuit = await wasm_tester(
+      "contracts/circuits/MastermindVariation.circom"
+    );
+    await circuit.loadConstraints();
+    const INPUT = {
+      pubGuessA: 2,
+      pubGuessB: 3,
+      pubGuessC: 4,
+      pubNumHit: 0,
+      pubNumBlow: 2,
+      pubSolnHash: F.toObject(pubSolnHash),
+      privSolnA: privSolnA,
+      privSolnB: privSolnB,
+      privSolnC: privSolnC,
+      privSalt: privSalt,
+    };
+
+    const witness = await circuit.calculateWitness(INPUT, true);
+
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+  });
+});
--- Part2/src/index.ts
+++ Part2/src/index.ts
@@ -239,6 +239,21 @@ const encrypt = async (
 ): Promise<Ciphertext> => {
   const mimc7 = await buildMimc7();
   // [assignment] generate the IV, use Mimc7 to hash the shared key with the IV, then encrypt the plain text
+
+  // converting multi hash to bigint
+  const iv = buf2Bigint(mimc7.multiHash(plaintext, BigInt(0)));
+
+  // ciphertext calculation from shared key and iv
+  const ciphertext: Ciphertext = {
+    iv,
+    data: plaintext.map(
+      (e: bigint, i: number): bigint => {
+        return e+buf2Bigint(mimc7.hash(sharedKey, iv+BigInt(i)))
+      }
+    )
+  };
+
+  return ciphertext;
 };
 
 /*
@@ -250,6 +265,16 @@ const decrypt = async (
   sharedKey: EcdhSharedKey,
 ): Promise<Plaintext> => {
   // [assignment] use Mimc7 to hash the shared key with the IV, then descrypt the ciphertext
+  const mimc7 = await buildMimc7();
+
+  // decryption
+  const plaintext: Plaintext = ciphertext.data.map(
+    (e: bigint, i: number): bigint => {
+      return (e - buf2Bigint(mimc7.hash(sharedKey, ciphertext.iv+BigInt(i))))
+    }
+  );
+
+  return plaintext;
 };
 
 export {
